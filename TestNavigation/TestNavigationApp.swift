83af4e074e58fd0d31dc4e6671ccf180694d2b25
srinivasrowdur <99046278+srinivasrowdur@users.noreply.github.com>
2022-11-18 23:35:35 +0000

    Initial Commit

---
+++ TestNavigation/TestNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestNavigationApp.swift
+//  TestNavigation
+//
+//  Created by Srinivas Rowdur on 11/18/22.
+//
+
+import SwiftUI
+
+@main
+struct TestNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
